2c69091b2729aca3bf9d3b1cd07c1b5fc81a7e20
edywill <edywill@hotmail.com>
2015-05-12 14:04:30 -0300

    Atualização do registro de BP

--- projetocpb/prestcont/passagens/registraBdpass.php
+++ projetocpb/prestcont/passagens/registraBdpass.php
@@ -1,10 +1,18 @@
 <?php 
 $itens=$_REQUEST['bdpass'];
 require "../../conexaomysql.php";
+require "../../conectsqlserverciprod.php";
 $valida=0;
 $cont=count($itens);
 foreach($itens AS $item) {
    $sql=mysql_query("UPDATE registros SET bdpass=1 WHERE id='".$item."'");
+   $sqlDeletePendencia=mysql_query("DELETE FROM prestbloqueados WHERE idaut='".$item."'");
+   $sqlDados=mysql_fetch_array(mysql_query("SELECT idben FROM registros WHERE id='".$item."'"));
+   $sqlCountDados=mysql_num_rows(mysql_query("SELECT cdempres FROM prestbloqueados WHERE cdempres='".$sqlDados['idben']."'"));
+   if($sqlCountDados<1){
+	   $sqlDeleteCigam=odbc_exec($conCab,"DELETE FROM TE_BLOQUEIOBPASS WHERE Empresa='".$sqlDados['idben']."'");
+	   }
+   
 	if($sql){
 		$valida++;
 		}
